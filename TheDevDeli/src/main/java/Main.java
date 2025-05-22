8bc2fce800334a35973dec860ec45685162255d0
krosengr4 <rosenkev4@gmail.com>
2025-05-22 15:57:14 -0500

    Created Main class with empty Main method. Commented the list of requirements. #2

---
+++ TheDevDeli/src/main/java/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    /*
+    *** An application for a Sandwich Shop where customers can fully
+        customize their sandwich orders. ***
+
+    * Customers can order a sandwich in 3 sizes: 4", 8" and 12"
+    *
+    * When they order, they choose the type of bread (white, wheat, rye, or wrap)
+    *
+    * Then a customer can choose their toppings.
+    * Toppings are categorized as regular or premium.
+    * Premium Toppings: Meats and Cheeses.
+    * Regular Toppings: most other toppings that aren't meats or cheeses.
+    * A customer can request extra toppings. Premium toppings have an additional cost.
+    *
+    * A customer has the option for the sandwich to be toasted.
+    *
+    * A customer has the option to add chips and a drink to the order.
+    *
+    * When the customer completes their order, display the
+    * order details (sandwiches ordered w/ toppings, if toasted or not, if they added
+    * chips or a drink).
+    * Verify with the customer that the order is correct.
+    * This should also display the total cost.
+    *
+    * When the customer completes the order, the order details are saved to its own
+    * receipt .txt file.
+    * The file name is named by the date and time the order was placed.
+    * Format: yyyyMMdd-hhmmss.txt (ex: 20230329-121523.txt)
+    * The .txt file will then be saved and kept in a receipts' folder.
+    *
+    * The application must include several screens with the features listen in workbook.
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
