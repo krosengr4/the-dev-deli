212a77192ae5d12c1309b4270e9432f42ee62a8e
krosengr4 <rosenkev4@gmail.com>
2025-05-23 15:13:11 -0500

    Moved Home Screen display to the Main class. Replaced UserInterface class with DeliStore class

--- TheDevDeli/src/main/java/DeliStore.java
+++ TheDevDeli/src/main/java/DeliStore.java
@@ -1,5 +1,56 @@
 public class DeliStore {
 
+    public void displayOrderScreen() {
+        boolean ifContinueOrder = true;
 
+        do {
+            System.out.printf("""
+                                                      ORDER
+                                               ---------------------
+                                1 - Order a Sandwich              3 - Add a Drink
+                                2 - Add Chips                     4 - Checkout
+                    
+                                              0 - Cancel Order
+                    """);
+
+            int userAction = Utils.messageAndResponseInt("Enter Your Option: ");
+
+            if (userAction < 0 || userAction > 4) {
+                System.err.println("ERROR! Please enter a number that is listed!");
+            } else if (userAction == 0) {
+                ifContinueOrder = false;
+            } else {
+                processOrder(userAction);
+            }
+
+        } while (ifContinueOrder);
+    }
+
+    private void processOrder(int userAction) {
+
+        //no default because error was handled in display method
+        switch (userAction) {
+            case 1 -> processAddSandwich();
+            case 2 -> processAddChips();
+            case 3 -> processAddDrink();
+            case 4 -> processCheckout();
+        }
+    }
+
+    private void processAddSandwich() {
+        System.out.println("Add a sandwich");
+    }
+
+    private void processAddChips() {
+        System.out.println("Add chips");
+    }
+
+    private void processAddDrink() {
+        System.out.println("Add a drink");
+    }
+
+    private void processCheckout() {
+        System.out.println("Checkout");
+    }
 
 }
--- TheDevDeli/src/main/java/Main.java
+++ TheDevDeli/src/main/java/Main.java
@@ -34,8 +34,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        UserInterface userInterface = new UserInterface();
-        userInterface.displayHomeScreen();
+            boolean ifContinue = true;
 
+            do {
+                DeliStore devDeli = new DeliStore();
+
+                System.out.printf("""
+                                         \s
+                                         %s%s%sWELCOME TO DEV DELI%s%s%s
+                         Serving Developers an Extensive Library of Incredible Sandwiches.\s
+                                                 Have a Byte!  \s
+                     ______________________________________________________________________________
+                    \s
+                                                   OPTIONS:
+                                         ---------------------------
+                                    1 - Place an Order        0 - Exit
+                    
+                    \s""", Utils.sandwich, Utils.sandwich, Utils.sandwich, Utils.sandwich, Utils.sandwich, Utils.sandwich);
+                int userAction = Utils.messageAndResponseInt("Enter 1 or 0: ");
+
+                switch (userAction) {
+                    case 1 -> devDeli.displayOrderScreen();
+                    case 0 -> ifContinue = false;
+                    default -> System.err.println("ERROR! Please enter a number that is listed!");
+                }
+
+            } while (ifContinue);
+            System.out.println("\n\n\t\t\t\tThanks for coming into Dev Deli!" + Utils.sandwich + Utils.fire);
+            System.out.println("\t\t\t\tWe will see you soon!");
     }
 }
