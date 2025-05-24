a8ac656a8e97afc5accf2f82bf22dfa0787b5f35
krosengr4 <rosenkev4@gmail.com>
2025-05-23 22:02:19 -0500

    Created method to display sandwich menu in DeliStore class

--- TheDevDeli/src/main/java/DeliStore.java
+++ TheDevDeli/src/main/java/DeliStore.java
@@ -4,7 +4,7 @@ public class DeliStore {
         boolean ifContinueOrder = true;
 
         do {
-            System.out.printf("""
+            System.out.print("""
                                                       ORDER
                                                ---------------------
                                 1 - Order a Sandwich              3 - Add a Drink
@@ -30,15 +30,38 @@ public class DeliStore {
 
         //no default because error was handled in display method
         switch (userAction) {
-            case 1 -> processAddSandwich();
+            case 1 -> displaySandwichMenu();
             case 2 -> processAddChips();
             case 3 -> processAddDrink();
             case 4 -> processCheckout();
         }
     }
 
+    private void displaySandwichMenu() {
+        System.out.println("\n\t\t\t\t" + Utils.sandwich + "SANDWICH MENU" + Utils.sandwich);
+        Utils.printDivider("-" , 70);
+        System.out.printf("""
+                        BREADS %s      MEATS %s      CHEESES %s     TOPPINGS         SAUCES \s
+                -----------------------------------------------------------------------------------------
+                        White           Steak        American        Lettuce          Mayo
+                        Wheat           Ham          Provolone       Peppers          Mustard
+                        Rye             Salami       Cheddar         Onions           Ketchup
+                        Wrap            Roast Beef   Swiss           Tomatoes         Ranch
+                                        Chicken                      Jalapeños        Thousand Islands
+                                        Bacon                        Cucumbers        Vinaigrette
+                                                                     Pickles          Au Jus
+                                                                     Guacamole
+                                                                     Mushrooms
+                -----------------------------------------------------------------------------------------
+                SIZES               4"                  8"                  12"
+                """, Utils.bread, Utils.meat, Utils.cheese);
+        //todo display pricing somewhere around here
+
+        processAddSandwich();
+    }
+
     private void processAddSandwich() {
-        System.out.println("Add a sandwich");
+        System.out.println("Hot hot sammich comin' ur way sir");
     }
 
     private void processAddChips() {
