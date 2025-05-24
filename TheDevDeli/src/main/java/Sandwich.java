4dad771942845e0ce822f99afd0c3bc156cb014a
krosengr4 <rosenkev4@gmail.com>
2025-05-23 22:32:56 -0500

    Created constructor for Sandwich Class that only takes size. Created getBaseValue method to calc base price based on size. Created test for this method

--- TheDevDeli/src/main/java/Sandwich.java
+++ TheDevDeli/src/main/java/Sandwich.java
@@ -1,2 +1,35 @@
-public class Sandwich {
+public class Sandwich extends MenuItem{
+
+    String size;
+    String bread;
+    String meat;
+    String cheese;
+    String toppings;
+    String sauces;
+    boolean isExtraMeat;
+    boolean isExtraCheese;
+
+    public Sandwich (String size) {
+        super("Sandwich", 0.0);
+        this.size = size;
+    }
+
+
+
+    public double getBasePrice() {
+        double value = 0.0;
+
+        switch (this.size) {
+            case "SMALL" -> value += 5.5 + 1.00 + .75;
+            case "MEDIUM" -> value += 7.00 + 2.00 + 1.50;
+            case "LARGE" -> value += 8.50 + 3.00 + 2.25;
+            default -> System.out.println("ERROR! Size needs to be SMALL, MEDIUM, OR LARGE!");
+        }
+        return value;
+    }
+
+
+    public double getValue() {
+        return 0.0;
+    }
 }
---
+++ TheDevDeli/src/test/java/SandwichTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SandwichTest {
+
+
+    @Test
+    void getBasePrice() {
+
+        Sandwich sandwich = new Sandwich(Size.MEDIUM.toString());
+
+        double actual = sandwich.getBasePrice();
+
+        assertEquals(10.50, actual);
+    }
+
+    @Test
+    void getValue() {
+    }
+}
