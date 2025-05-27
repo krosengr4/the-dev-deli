9e06b45392faad3eaba95f3af72410adda9b21d3
krosengr4 <rosenkev4@gmail.com>
2025-05-27 12:08:44 -0500

    Created getters and setters for all sandwich attributes

--- TheDevDeli/src/main/java/Sandwich.java
+++ TheDevDeli/src/main/java/Sandwich.java
@@ -16,7 +16,71 @@ public class Sandwich extends MenuItem{
         this.size = size;
     }
 
+    //region setters and getters
+    public String getSize() {
+        return size;
+    }
 
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
+
+    public String getMeat() {
+        return meat;
+    }
+
+    public void setMeat(String meat) {
+        this.meat = meat;
+    }
+
+    public String getCheese() {
+        return cheese;
+    }
+
+    public void setCheese(String cheese) {
+        this.cheese = cheese;
+    }
+
+    public String getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String toppings) {
+        this.toppings = toppings;
+    }
+
+    public String getSauces() {
+        return sauces;
+    }
+
+    public void setSauces(String sauces) {
+        this.sauces = sauces;
+    }
+
+    public boolean isExtraMeat() {
+        return isExtraMeat;
+    }
+
+    public void setExtraMeat(boolean extraMeat) {
+        isExtraMeat = extraMeat;
+    }
+
+    public boolean isExtraCheese() {
+        return isExtraCheese;
+    }
+
+    public void setExtraCheese(boolean extraCheese) {
+        isExtraCheese = extraCheese;
+    }
+    //endregion
 
     public double getBasePrice() {
 //        value = 0.0;
