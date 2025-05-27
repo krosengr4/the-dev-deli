9e51cb7d6dbb252fd18bba56c06236e4bd4e8d2e
krosengr4 <rosenkev4@gmail.com>
2025-05-27 11:59:21 -0500

    Created message outline for if a user wants to add a sandwich

--- TheDevDeli/src/main/java/DeliStore.java
+++ TheDevDeli/src/main/java/DeliStore.java
@@ -43,12 +43,12 @@ public class DeliStore {
         System.out.printf("""
                         BREADS %s      MEATS %s      CHEESES %s     TOPPINGS         SAUCES \s
                 -----------------------------------------------------------------------------------------
-                        White           Steak        American        Lettuce          Mayo
-                        Wheat           Ham          Provolone       Peppers          Mustard
-                        Rye             Salami       Cheddar         Onions           Ketchup
-                        Wrap            Roast Beef   Swiss           Tomatoes         Ranch
-                                        Chicken                      Jalapeños        Thousand Islands
-                                        Bacon                        Cucumbers        Vinaigrette
+                        White          Steak         American        Lettuce          Mayo
+                        Wheat          Ham           Provolone       Peppers          Mustard
+                        Rye            Salami        Cheddar         Onions           Ketchup
+                        Wrap           Roast Beef    Swiss           Tomatoes         Ranch
+                                       Chicken                       Jalapeños        Thousand Islands
+                                       Bacon                         Cucumbers        Vinaigrette
                                                                      Pickles          Au Jus
                                                                      Guacamole
                                                                      Mushrooms
@@ -61,7 +61,33 @@ public class DeliStore {
     }
 
     private void processAddSandwich() {
-        System.out.println("Hot hot sammich comin' ur way sir");
+        System.out.println("1 - Small  |  2 - Medium  |  3 - Large");
+        int userSize = Utils.messageAndResponseInt("Please enter sandwich size: ");
+
+
+        String userBread = Utils.promptGetUserInput("Enter the type of bread you would like: ").trim();
+
+        String userMeat = Utils.promptGetUserInput("Enter the kind of meat you would like: ").trim();
+        String userExtraMeat = Utils.promptGetUserInput("Would you like extra meat? Y or N: ").trim();
+
+
+        int ifUserToppings = Utils.messageAndResponseInt("Would you like any other toppings?\n1 - Yes 2 - No: ");
+        if (ifUserToppings == 1) {
+
+            boolean ifMoreToppings = true;
+
+            while (ifMoreToppings) {
+                String userToppings = Utils.promptGetUserInput("Enter 1 topping you would like: ");
+
+                int extraUserToppings = Utils.messageAndResponseInt("Would you like any more toppings?\n1 - Yes 2 - No: ");
+                if (extraUserToppings > 2 || extraUserToppings < 1) {
+                    System.err.println("ERROR! Please enter a number between 1 and 2!");
+                } else if (extraUserToppings == 2) {
+                    ifMoreToppings = false;
+                }
+            }
+        }
+
     }
 
     private void processAddChips() {
