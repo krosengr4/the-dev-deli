ffdf803a6ada6dcb7dc4bba3d7ffabd8467a888f
krosengr4 <rosenkev4@gmail.com>
2025-05-27 21:44:05 -0500

    Added code in DeliStore class to allow user to choose what type of drink they would like

--- TheDevDeli/src/main/java/DeliStore.java
+++ TheDevDeli/src/main/java/DeliStore.java
@@ -22,7 +22,7 @@ public class DeliStore {
 
             switch (userAction) {
                 case 0 -> ifContinueOrder = false;
-                case 1 -> displaySandwichMenu();
+                case 1 -> processAddSandwich();
                 case 2 -> processAddChips();
                 case 3 -> processAddDrink();
                 case 4 -> {
@@ -35,18 +35,9 @@ public class DeliStore {
     }
 
 
-    private void displaySandwichMenu() {
+    private void processAddSandwich() {
 
         Sandwich sandwich = MakeSandwich.createSandwich();
-//        System.out.println("Sandwich size: " + sandwich.getSize());
-//        System.out.println("Sandwich Bread: " + sandwich.getBread());
-//        System.out.println("Sandwich meat: " + sandwich.getMeat());
-//        System.out.println("Extra Meat?: " + sandwich.isExtraMeat());
-//        System.out.println("Sandwich cheese: " + sandwich.getCheese());
-//        System.out.println("Extra Cheese?: " + sandwich.isExtraCheese());
-//        System.out.println("Sandwich toppings: " + sandwich.getToppings());
-//        System.out.println("Sandwich sauces: " + sandwich.getSauces());
-//        System.out.printf("Sandwich price: $%.2f\n", sandwich.getValue());
 
         customerOrder.add(sandwich);
 
@@ -73,29 +64,32 @@ public class DeliStore {
         while (addAnotherDrink) {
             Drink drink = new Drink();
 
-            System.out.println("\n1 - Small\n2 - Medium\n3 - Large");
+            System.out.println("\n----DRINK OPTIONS-----\n1 - Fountain Drink\n2 - Lemonade\n3 - Sweet Tea\n4 - Unsweetened Tea\n5 - MilkShake");
+            int userDrinkChoice = Utils.messageAndResponseInt("Select the number next to the desired drink: ");
+
+            switch (userDrinkChoice) {
+                case 1 -> drink.setName("Fountain Drink");
+                case 2 -> drink.setName("Lemonade");
+                case 3 -> drink.setName("Sweet Tea");
+                case 4 -> drink.setName("Unsweetened Tea");
+                case 5 -> drink.setName("MilkShake");
+                default -> System.err.println("ERROR! Please enter a number between 1 and 5!");
+            }
+
+            System.out.println("\n1 - Small $2.00\n2 - Medium $2.50\n3 - Large $3.00");
             int userDrinkSize = Utils.messageAndResponseInt("Enter a drink size: ");
 
             switch (userDrinkSize) {
-                case 1 -> {
-                    drink.setSize("SMALL");
-                    System.out.println("Success! Small drink added!");
-                }
-                case 2 -> {
-                    drink.setSize("MEDIUM");
-                    System.out.println("Success! Medium drink added!");
-                }
-                case 3 -> {
-                    drink.setSize("LARGE");
-                    System.out.println("Success! Large drink added!");
-                }
+                case 1 -> drink.setSize("SMALL");
+                case 2 -> drink.setSize("MEDIUM");
+                case 3 -> drink.setSize("LARGE");
                 default -> System.err.println("ERROR! Please enter a number between 1 and 3!");
             }
 
             customerOrder.add(drink);
-            Utils.pauseApp();
+            System.out.printf("\nSuccess! %s %s added!\n", drink.getSize().toLowerCase(), drink.getName());
 
-            String anotherDrink = Utils.promptGetUserInput("Would you like to add another drink? (Y or N): ").trim();
+            String anotherDrink = Utils.promptGetUserInput("\nWould you like to add another drink? (Y or N): ").trim();
             if (anotherDrink.equalsIgnoreCase("n")) {
                 addAnotherDrink = false;
             } else if (!anotherDrink.equalsIgnoreCase("y")) {
--- TheDevDeli/src/main/java/MenuItem.java
+++ TheDevDeli/src/main/java/MenuItem.java
@@ -11,16 +11,8 @@ public abstract class MenuItem {
         return name;
     }
 
-    public double getPrice() {
-        return price;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
     }
     //endregion
 
