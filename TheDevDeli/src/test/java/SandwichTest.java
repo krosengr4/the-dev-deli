60a3e5537131b39a86cef80e4fd69fb94dc913e3
krosengr4 <rosenkev4@gmail.com>
2025-05-28 10:34:21 -0500

    Created tests for small, medium, large sandwiches with normal meat and cheese

--- TheDevDeli/src/test/java/SandwichTest.java
+++ TheDevDeli/src/test/java/SandwichTest.java
@@ -4,19 +4,44 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SandwichTest {
 
+    Sandwich sandwich = new Sandwich();
 
     @Test
-    void getBasePrice() {
+    void getValueSmallNormalMeatAndCheese() {
 
-        Sandwich sandwich = new Sandwich();
-        sandwich.setSize(Size.MEDIUM.toString());
+        sandwich.setSize("SMALL");
+        sandwich.setBread("White");
+        sandwich.setMeat("Ham");
+        sandwich.setCheese("American");
 
-        double actual = sandwich.calculatePrice();
+        double actual = sandwich.getValue();
 
-        assertEquals(10.50, actual);
+        assertEquals(7.25, actual);
     }
 
     @Test
-    void getValue() {
+    void getValueMediumNormalMeatAndCheese() {
+
+        sandwich.setSize("MEDIUM");
+        sandwich.setBread("White");
+        sandwich.setMeat("Ham");
+        sandwich.setCheese("American");
+
+        double actual = sandwich.getValue();
+
+        assertEquals(10.5, actual);
+    }
+
+    @Test
+    void getValueLargeNormalMeatAndCheese() {
+
+        sandwich.setSize("LARGE");
+        sandwich.setBread("White");
+        sandwich.setMeat("Ham");
+        sandwich.setCheese("American");
+
+        double actual = sandwich.getValue();
+
+        assertEquals(13.75, actual);
     }
 }
