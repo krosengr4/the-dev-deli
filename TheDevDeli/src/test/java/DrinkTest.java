0df5191474f675f54fdfc662bc0eeef99dcc7807
krosengr4 <rosenkev4@gmail.com>
2025-05-28 10:51:27 -0500

    Created unit tests with comments for the value of different drink sizes

--- TheDevDeli/src/test/java/DrinkTest.java
+++ TheDevDeli/src/test/java/DrinkTest.java
@@ -4,7 +4,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DrinkTest {
 
+    //New instance of drink accessible to all test cases
+    Drink drink = new Drink();
+
+    //Value of small drink
     @Test
-    void getValue() {
+    void getValueSmallDrink() {
+
+        drink.setSize("SMALL");
+
+        double actual = drink.getValue();
+
+        assertEquals(2.00, actual);
     }
+
+    //Value of medium drink
+    @Test
+    void getValueMediumDrink() {
+
+        drink.setSize("MEDIUM");
+
+        double actual = drink.getValue();
+
+        assertEquals(2.50, actual);
+    }
+
+    //Value of large drink
+    @Test
+    void getValueLargeDrink() {
+
+        drink.setSize("LARGE");
+
+        double actual = drink.getValue();
+
+        assertEquals(3.00, actual);
+    }
+
 }
