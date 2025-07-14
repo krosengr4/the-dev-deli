9792a3e0991e79845289e95520e5fa75ea6093ce
krosengr4 <rosenkev4@gmail.com>
2025-07-14 17:46:01 -0500

    Created displayName methods so that the size  options are nicely formatted when printed out

--- TheDevDeli/src/main/java/models/enums/Size.java
+++ TheDevDeli/src/main/java/models/enums/Size.java
@@ -2,8 +2,22 @@ package models.enums;
 
 public enum Size {
 
-    SMALL,
-    MEDIUM,
-    LARGE
+    SMALL("Small"),
+    MEDIUM("Medium"),
+    LARGE("Large");
+
+	private final String displayName;
+	Size(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
 
 }
