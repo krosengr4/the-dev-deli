0ab80d99da48eeab94d058a6b16c42be3a52ba83
krosengr4 <rosenkev4@gmail.com>
2025-07-14 08:22:06 -0500

    Creaeted enums for all of the sandwich options, with methods to display their names nicely

---
+++ TheDevDeli/src/main/java/models/sandwich/Sandwich.java
@@ -0,0 +1,11 @@
+package models;
+
+import models.enums.Bread;
+import models.enums.Size;
+
+public class Sandwich {
+
+	Size size;
+	Bread bread;
+
+}
---
+++ TheDevDeli/src/main/java/models/sandwich/SandwichOptions.java
@@ -0,0 +1,75 @@
+package models.sandwich;
+
+enum Bread {
+	WHITE("White"), WHEAT("Wheat"), RYE("Rye"), WRAP("Wrap");
+
+	private final String displayName;
+	Bread(String displayName) {
+		this.displayName = displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
+}
+
+enum Meat {
+	STEAK("Steak"), HAM("Ham"), SALAMI("Salami"), ROASTBEEF("Roast Beef"), CHICKEN("Chicken"), BACON("Bacon"), NONE("No Meat");
+
+	private final String displayName;
+	Meat(String displayName) {
+		this.displayName = displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
+}
+
+enum Cheese {
+	AMERICAN("American"), PROVOLONE("Provolone"), CHEDDAR("Cheddar"), SWISS("Swiss"), NONE("No Cheese");
+
+	private final String displayName;
+	Cheese(String displayName) {
+		this.displayName = displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
+}
+
+enum Topping {
+	LETTUCE("Lettuce"), PEPPERS("Peppers"), ONIONS("Onions"), TOMATOES("Tomatoes"), JALAPENOS("Jalapenos"), CUCUMBERS("Cucumbers"),
+	PICKLES("Pickles"), GUACAMOLE("Guacamole"), MUSHROOMS("Mushrooms"), NONE("No Topping");
+
+	private final String displayName;
+	Topping(String displayName) {
+		this.displayName = displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
+}
+
+enum Sauce {
+	MAYO("Mayo"), MUSTARD("Mustard"), KETCHUP("Ketchup"), RANCH("Ranch"), THOUSANDISLANDS("Thousand Islands"), VINAIGRETTE("Vinaigrette"), AUJUS("Au Jus"), NONE("No Sauce");
+
+	private final String displayName;
+	Sauce(String displayName) {
+		this.displayName = displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
+}
+
+
+
