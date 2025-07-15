fee622027b3efee2dd83de50b3870839b24efb0f
krosengr4 <rosenkev4@gmail.com>
2025-07-15 09:36:01 -0500

    Created a class to handle the Main Screen logic

--- TheDevDeli/src/main/java/logic/MainLogic.java
+++ TheDevDeli/src/main/java/logic/MainLogic.java
@@ -1,4 +1,29 @@
 package logic;
 
+import ui.UserInterface;
+
 public class MainLogic {
+
+	private static final UserInterface ui = new UserInterface();
+
+	public void processMainScreen() {
+		boolean ifContinue = true;
+
+		while(ifContinue) {
+			int userChoice = ui.displayMainScreen();
+
+			switch(userChoice) {
+				case 1 -> processOrderOption();
+				case 0 -> ifContinue = false;
+				default -> System.err.println("ERROR! Please enter a number that is listed!");
+			}
+		}
+	}
+
+	private static void processOrderOption() {
+
+	}
+
+
+
 }
