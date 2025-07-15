a4f40754536f6f06e25d0273be63369e322c2e97
krosengr4 <rosenkev4@gmail.com>
2025-07-15 11:41:03 -0500

    Created methods in Main logic class to process what a user wants to order

--- TheDevDeli/src/main/java/logic/MainLogic.java
+++ TheDevDeli/src/main/java/logic/MainLogic.java
@@ -2,9 +2,12 @@ package logic;
 
 import ui.UserInterface;
 
-public class MainLogic {
+public class MainLogic extends LogicBase{
 
-	private static final UserInterface ui = new UserInterface();
+//	private static final UserInterface ui = new UserInterface();
+	public MainLogic(UserInterface ui) {
+		super(ui);
+	}
 
 	public void processMainScreen() {
 		boolean ifContinue = true;
@@ -21,6 +24,22 @@ public class MainLogic {
 	}
 
 	private static void processOrderOption() {
+		boolean ifContinue = true;
+
+		while(ifContinue) {
+			int userChoice = ui.displayOrderScreen();
+
+			switch(userChoice) {
+				case 1 -> SandwichLogic.processSandwichMenu();
+				case 2 -> ChipLogic.processChipOption();
+				case 3 -> DrinkLogic.processDrinkOption();
+				case 4 -> {
+					CheckoutLogic.processCheckout();
+					ifContinue = false;
+				}
+				case 0 -> ifContinue = false;
+			}
+		}
 
 	}
 
