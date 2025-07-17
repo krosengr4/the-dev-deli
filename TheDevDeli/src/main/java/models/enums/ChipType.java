816c8178c78c525283161a578b7a0072be4f6d14
krosengr4 <rosenkev4@gmail.com>
2025-07-16 21:30:58 -0500

    Added methods to display the chip type and drink type nicely in the Drink and Chip type enums

--- TheDevDeli/src/main/java/models/enums/ChipType.java
+++ TheDevDeli/src/main/java/models/enums/ChipType.java
@@ -2,12 +2,21 @@ package models.enums;
 
 public enum ChipType {
 
-	FRENCHFRIES,
-	DORITOS,
-	JALAPENO,
-	LAYS,
-	CHEETOS,
-	TAKIS,
-	CHEEZITS
+	FRENCHFRIES("French Fries"), DORITOS("Nacho Cheese Doritos"), MISS_VICKIES_JALAPENO("Miss Vickies Jalapeno Chips"), LAYS_ORIGINAL("Lays Original Potato Chips"),
+	CHEETOS("Cheetos"), TAKIS("Takis"), CHEEZITS("Cheez-Its");
+
+	private final String displayName;
+	ChipType(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
 
 }
--- TheDevDeli/src/main/java/models/enums/DrinkType.java
+++ TheDevDeli/src/main/java/models/enums/DrinkType.java
@@ -2,12 +2,20 @@ package models.enums;
 
 public enum DrinkType {
 
-	FOUNTAIN,
-	LEMONADE,
-	SWEETTEA,
-	MILKSHAKE,
-	JUICE,
-	CATPISS,
-	WATER
+	FOUNTAIN("Fountain Drink"), LEMONADE("Lemonade"), SWEET_TEA("Sweet Tea"), UNSWEET_TEA("Unsweet Tea"), MILKSHAKE("Milkshake"), JUICE("Juice"), WATER("Water");
+
+	private final String displayName;
+	DrinkType(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
 
 }
