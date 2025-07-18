14c088f23bc42049e6c8c948319ff83ae95a137b
krosengr4 <rosenkev4@gmail.com>
2025-07-18 01:10:25 -0500

    Added method in sandwich logic for user to select size of sandwich

--- TheDevDeli/src/main/java/logic/SandwichLogic.java
+++ TheDevDeli/src/main/java/logic/SandwichLogic.java
@@ -1,13 +1,14 @@
 package logic;
 
 import models.MenuItem;
+import models.enums.Size;
 import models.sandwich.Sandwich;
 import ui.UserInterface;
 import utils.Utils;
 
 import java.util.List;
 
-public class SandwichLogic extends LogicBase{
+public class SandwichLogic extends LogicBase {
 
 	public SandwichLogic(UserInterface ui, List<MenuItem> customerOrder) {
 		super(ui, customerOrder);
@@ -29,23 +30,36 @@ public class SandwichLogic extends LogicBase{
 
 	private static void createNewSandwich() {
 		boolean ifContinue = true;
+		// Build sandwich by calling methods to add item on sandwich
+
+
 		Sandwich sandwich = new Sandwich();
 	}
 
+	private static Sandwich selectSide() {
+		Sandwich sandwich = new Sandwich();
+		int userChoice = ui.displaySandwichSizes();
+
+		switch(userChoice) {
+			case 1 -> sandwich.setSize(Size.SMALL);
+			case 2 -> sandwich.setSize(Size.MEDIUM);
+			case 3 -> sandwich.setSize(Size.LARGE);
+			default -> System.err.println("ERROR! Please enter a choice that is listed!!!");
+		}
+
+		return sandwich;
+	}
+
 	private static void addToOrder(Sandwich sandwich) {
-		boolean ifContinue = true;
+		int userConfirmation = ui.confirmSandwich(sandwich);
 
-		while(ifContinue) {
-			int userConfirmation = ui.confirmSandwich(sandwich);
-
-			if(userConfirmation == 1) {
-				customerOrder.add(sandwich);
-				System.out.println(Utils.GREEN + "Your sandwich was added to the order!" + Utils.RESET);
-				Utils.playSound("owin31");
-			} else {
-				System.out.println("Apologies for the inconvenience. Please try again!");
-				Utils.playSound("womp-womp");
-			}
+		if(userConfirmation == 1) {
+			customerOrder.add(sandwich);
+			System.out.println(Utils.GREEN + "Your sandwich was added to the order!" + Utils.RESET);
+			Utils.playSound("owin31");
+		} else {
+			System.out.println("Apologies for the inconvenience. Please try again!");
+			Utils.playSound("womp-womp");
 		}
 	}
 
