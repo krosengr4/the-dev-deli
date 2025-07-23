01624b1ba67295bb0cc598e1a40a646784322709
krosengr4 <rosenkev4@gmail.com>
2025-07-23 07:08:26 -0500

    Added Logic method to allow admin to update an order

--- TheDevDeli/src/main/java/logic/AdminLogic.java
+++ TheDevDeli/src/main/java/logic/AdminLogic.java
@@ -69,7 +69,29 @@ public class AdminLogic {
 	}
 
 	private static void processUpdateOrder() {
+		int orderId = Utils.getUserInputInt("Enter the order ID of the order to update: ");
 
+		Order order = orderDao.getById(orderId);
+		if(order == null) {
+			System.out.println("There are no orders with that ID...");
+		} else {
+			int userChoice = adminUI.displayUpdateOptions();
+
+			switch(userChoice) {
+				case 1 -> {
+					String newName = Utils.getUserInput("Enter the new name for the customer: ");
+					order.setCustomerName(newName);
+					orderDao.updateOrder(order);
+				}
+				case 2 -> {
+					double newAmount = Utils.getUserInputDouble("Enter the new amount for the order: ");
+					order.setTotalPrice(newAmount);
+					orderDao.updateOrder(order);
+				}
+			}
+		}
+
+		Utils.pauseApp();
 	}
 
 	private static void processDeleteOrder() {
